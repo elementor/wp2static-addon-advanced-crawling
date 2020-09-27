bd13ff235ac1fac1aabe663a7f1c9f5b8958aff6
Leon Stafford <me@ljs.dev>
2020-09-28 06:17:29 +0930

    add build script

---
+++ tools/build_release.sh
@@ -0,0 +1,54 @@
+#!/bin/bash
+
+####################################################
+##
+## Build WP2Static Advanced Crawler Deployment Addon
+##
+## script archive_name dont_minify
+##
+## places archive in $HOME/Downloads
+##
+####################################################
+
+# run script from project root
+EXEC_DIR=$(pwd)
+
+TMP_DIR=$HOME/plugintmp
+rm -Rf $TMP_DIR
+mkdir -p $TMP_DIR
+
+rm -Rf $TMP_DIR/wp2static-addon-advanced-crawler
+mkdir $TMP_DIR/wp2static-addon-advanced-crawler
+
+# clear dev dependencies
+rm -Rf $EXEC_DIR/vendor/*
+# load prod deps and optimize loader
+composer install --quiet --no-dev --optimize-autoloader
+
+# cp all required sources to build dir
+cp -r $EXEC_DIR/*.php $TMP_DIR/wp2static-addon-advanced-crawler/
+cp -r $EXEC_DIR/src $TMP_DIR/wp2static-addon-advanced-crawler/
+cp -r $EXEC_DIR/vendor $TMP_DIR/wp2static-addon-advanced-crawler/
+cp -r $EXEC_DIR/README.txt $TMP_DIR/wp2static-addon-advanced-crawler/
+cp -r $EXEC_DIR/views $TMP_DIR/wp2static-addon-advanced-crawler/
+
+cd $TMP_DIR
+
+# tidy permissions
+find . -type d -exec chmod 755 {} \;
+find . -type f -exec chmod 644 {} \;
+
+zip -r -9 ./$1.zip ./wp2static-addon-advanced-crawler
+
+cd -
+
+mkdir -p $HOME/Downloads/
+
+cp $TMP_DIR/$1.zip $HOME/Downloads/
+
+# reset dev dependencies
+cd $EXEC_DIR
+# clear dev dependencies
+rm -Rf $EXEC_DIR/vendor/*
+# load prod deps
+composer install --quiet
